9613454e391137081a4fa30106e7eec26625d75d
pratikdevnani <pratikdevnani24@gmail.com>
2023-03-29 17:34:52 -0700

    basic flask app

---
+++ src/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p> HELLO FROM THE APP! </p>"
+
+@app.route("/health")
+def health():
+    return jsonify(
+        status = "UP"
+    )
+
+if __name__ == '__main__':
+    app.run(host = "0.0.0.0", port = 80)
